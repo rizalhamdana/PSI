d485612b9014e0599e36db53cb49eb3881dda88f
rizalhamdana <rizalhamdan.sdw@gmail.com>
2018-04-11 12:13:52 +0700

    tambah view login

--- application/views/v_login.php
+++ application/views/v_login.php
@@ -2,8 +2,29 @@
 <html>
 <head>
 	<title>Login</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
-
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 col-md-offset-4">
+				<div class="text-center" style="margin-top: 200px;">	
+					<h1>Halaman Login</h1>
+					<form action="" method="post">
+						<p><input type="text" name="username" placeholder="Username" class="form-control"></p>
+						<p><input type="password" name="username" placeholder="Password" class="form-control"></p>
+						<input type="submit" class="btn btn-success" style="width: 100%">
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
