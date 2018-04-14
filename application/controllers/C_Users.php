876c6ec59312050811877bd0dba983f460a6ed8d
Saputro Andi <sptandi@gmail.com>
2018-04-14 20:18:38 +0700

    add sql table

--- application/controllers/C_Users.php
+++ application/controllers/C_Users.php
@@ -8,9 +8,14 @@ class C_Users extends CI_Controller
 	function __construct()
 	{
 		parent::__construct();
+		$this->load->helper('url');
 	}
 	public function index(){
 		$this->load->view('v_login');
+	}
+
+	public function dashboard() {
+		$this->load->view('user/dashboard');
 	}
 	
 }
---
+++ application/views/user/dashboard.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/user/dashboard.css">
+</head>
+<body>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a href="#" class="navbar-brand">Logo</a>
+			</div>
+			<ul class="nav navbar-nav">
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href=""><span class="glyphicon glyphicon-user"></span></a></li>
+				<li><a href=""><span class="glyphicon glyphicon-log-in"></span></a></li>
+			</ul>
+		</div>
+	</nav>
+
+	<div class="container-fluid">
+	  <div class="row">
+	    	<div class="col-sm-3 col-lg-2">
+	      		<nav class="navbar navbar-default navbar-fixed-left">
+			        <li class="sidebar1">Menu1</li>
+			        <li class="sidebar2">Menu2</li>
+			        <li class="sidebar3">Menu3</li>
+				</nav>
+			</div>
+	  		<div class="col-sm-9 col-lg-10 content">
+	      <!-- your page content -->
+	  		</div>
+	 	</div>
+	</div>
+</body>
+</html>
