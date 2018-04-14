f6eabca956f8515c82d0eb6ada6047bb59d5cfb3
rizalhamdana <rizalhamdan.sdw@gmail.com>
2018-04-14 20:59:09 +0700

    terbaru

--- application/controllers/C_Users.php
+++ application/controllers/C_Users.php
@@ -13,17 +13,16 @@ class C_Users extends CI_Controller
 	public function index(){
 		$this->load->view('v_login');
 	}
-<<<<<<< HEAD
+
 	public function login(){
 		$username=$this->input->post('username');
 		$password=$this->input->post('password');
 		echo $username."<br>";
 		echo $password;
-=======
+	}
 
 	public function dashboard() {
 		$this->load->view('user/dashboard');
->>>>>>> 876c6ec59312050811877bd0dba983f460a6ed8d
 	}
 	
 }
