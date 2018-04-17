3ce682c9b035cd394821352499c543b61c3ca003
rizalhamdana <rizalhamdan.sdw@gmail.com>
2018-04-18 00:26:45 +0700

    ubah route['dasboard']

--- application/config/routes.php
+++ application/config/routes.php
@@ -4,4 +4,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'C_Users';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['dashboard'] = 'C_users/dashboard';
+$route['dashboard'] = 'C_Bencana/index';
