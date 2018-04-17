0fe6b66ef75f5634b85f49fd4e77c127bba137b7
rizalhamdana <rizalhamdan.sdw@gmail.com>
2018-04-18 00:47:13 +0700

    tambah responsive halaman login

--- application/controllers/C_Bencana.php
+++ application/controllers/C_Bencana.php
@@ -9,12 +9,15 @@ class C_Bencana extends CI_Controller
 	function __construct()
 	{
 		parent::__construct();
+		$this->load->helper('url');
 
 	}
 	function index(){
 		if($this->session->has_userdata('username')){
 			$userAktif=$this->session->userdata('nama_pengguna');
 			$this->load->view('user/v_dashboard',compact('userAktif'));	
+		}else{
+			redirect('C_Users');
 		}
 		
 	}
--- application/views/user/v_login.php
+++ application/views/user/v_login.php
@@ -14,7 +14,7 @@
 <body>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-4 col-md-offset-4">
+			<div class="col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-8 col-xs-offset-2">
 				<div class="text-center" style="margin-top: 200px;">	
 					<h1>Halaman Login</h1>
 					<form action="<?= base_url('C_Users/login')?>" method="post">
