804acbc22fd4106c2656fa4ea0e413bbf71bf45b
rizalhamdana <rizalhamdan.sdw@gmail.com>
2018-06-19 11:50:08 +0700

    ubah tampilan input laporan dan detail bencana untuk pelapor

--- application/views/user/v_detail_bencana_for_pelapor.php
+++ application/views/user/v_detail_bencana_for_pelapor.php
@@ -8,81 +8,55 @@
 		color: 	#A7A1AE;
 	}
 </style>
-<body class="dashboard-pelapor">
-<nav class="navbar navbar-default navbar-static-top m-b-0" >
-            <div class="navbar-header" style="background-color: #4286f4">
-                
-                <!-- /Logo -->
-                <ul class="nav navbar-top-links navbar-right pull-right">
-                    <li>
-                        <a class="profile-pic" href="<?= base_url('C_Users/logout');?>"><b class="hidden-xs">Logout</b></a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-header -->
-            <!-- /.navbar-top-links -->
-            <!-- /.navbar-static-side -->
-        </nav>
-<div class="container" style="padding: 50px 0;">
-	<a href="<?php echo base_url('C_PelaporBencana')?>" class="btn btn-primary">Kembali ke Halaman Utama</a>
-	<div class="row">
-		<?php 
-			foreach($result as $bencana){?>		
-				<div class="col col-md-12">
-					<div class="col-md-6 col-md-offset-3 text-center">
-						<h1 style=" color: #4DC3FA;">
-							<?php 
-
-								echo $bencana->nama_bencana; 
-							?>
-						</h1>	
-						<p><?php echo $bencana->tanggal_bencana;?></p>	
-					</div>
-					
+<body class="fix-header">
+	 <?php $this->load->view('menu_nav') ?>
+	<div id="wrapper">
+		<div id="page-wrapper">
+			<div class="container-fluid">
+				<div class="row bg-title">
+					<?php foreach ($result as $bencana): ?>
+						<div class="col-lg-3 col-md-4 col-sm-4 col-xs-12 col-md-offset-4 text-center">
+                        	<h4 class="page-title"><?= $bencana->nama_bencana; ?></h4> 
+                        	<p><?php echo $bencana->tanggal_bencana;?></p>
+                   		</div>
+					<?php endforeach ?>
 				</div>
-				
-		<?php } ?>
-	</div>
-	<div class="container">
-		<div class="row">	
-		<div class="col col-md-12 col-sm-12">	
-			<h2 style="font-weight: bolder; margin-bottom: 50px; color: white;">Laporan Anda</h2>
-			<div class="table-responsive" style="overflow: auto;">
-			<table class="table">	
-				<thead class="table-cell dark-navy" style="">	
-						<tr>	
-							<th scope="col">Lokasi</th>
-							<th scope="col">Objek Kerusakan</th>
-							<th scope="col">Jenis Kerusakan</th>
-							<th scope="col">Tanggal dan Waktu Lapor</th>
-							<th scope="col">Aksi</th>
-						</tr>
-				</thead>
-				<tbody class="table-cell">	
+				<div class="row">
+					<div class="white-box">
+						<h2>Laporan Anda</h2>
+						<div class="table-responsive" style="overflow: auto">
+							<table class="table">	
+								<thead class="table-cell dark-navy" style="">	
+									<tr>	
+										<th scope="col">Lokasi</th>
+										<th scope="col">Objek Kerusakan</th>
+										<th scope="col">Jenis Kerusakan</th>
+										<th scope="col">Tanggal dan Waktu Lapor</th>
+										<th scope="col">Aksi</th>
+									</tr>
+								</thead>
+								<tbody class="table-cell">	
 					
-						<?php foreach($hasil_semua_laporan as $laporan_user){?>
-							<tr>
-							<td><?php echo $laporan_user->nama_wilayah ?></td>
-							<td><?php echo $laporan_user->objek ?></td>
-							<td><?php echo $laporan_user->jenis_kerusakan ?></td>
-							<td><?php echo $laporan_user->tanggal_laporan ?></td>
-							<td><a href="<?= base_url('C_PelaporBencana/ubahLaporan/'.$laporan_user->id_bencana.'?id_laporan='.$laporan_user->id_laporan)?>" class="btn btn-warning">Ubah</a>&nbsp <a href="<?= base_url('C_PelaporBencana/hapusLaporan/'.$laporan_user->id_bencana. '?id_laporan='.$laporan_user->id_laporan)?>" class="btn btn-danger">Hapus</a></td>
-							</tr>
-						<?php } ?>
+								<?php foreach($hasil_semua_laporan as $laporan_user){?>
+									<tr>
+										<td><?php echo $laporan_user->nama_wilayah ?></td>
+										<td><?php echo $laporan_user->objek ?></td>
+										<td><?php echo $laporan_user->jenis_kerusakan ?></td>
+										<td><?php echo $laporan_user->tanggal_laporan ?></td>
+										<td><a href="<?= base_url('C_PelaporBencana/ubahLaporan/'.$laporan_user->id_bencana.'?id_laporan='.$laporan_user->id_laporan)?>" class="btn btn-warning">Ubah</a>&nbsp <a href="<?= base_url('C_PelaporBencana/hapusLaporan/'.$laporan_user->id_bencana. '?id_laporan='.$laporan_user->id_laporan)?>" class="btn btn-danger">Hapus</a></td>
+									</tr>
+								<?php } ?>
 				
-				</tbody>
+								</tbody>
 				
-			</table>
-			
+							</table>
+							
+						</div>
+						<a href="<?= base_url('C_PelaporBencana/tambahLaporan?id_bencana=').$id_bencana;?>" class="btn btn-primary">Tambah Laporan</a>
+					</div>
+				</div>
+			</div>
 		</div>
-		<a href="<?= base_url('C_PelaporBencana/tambahLaporan?id_bencana=').$id_bencana;?>" class="btn btn-primary">Tambah Laporan</a>
-		</div>
-
 	</div>
-	</div>
-	
-
-
-</div>
 </body>
 </html>
--- application/views/user/v_tambah_laporan.php
+++ application/views/user/v_tambah_laporan.php
@@ -9,21 +9,26 @@
  		margin: 10px 0px;
  	}
  </style>
- <body>
-
- 	<div class="container" style="margin-top:50px; margin-bottom: 50px;  ">
- 		<div class="row">
- 			<div class="col col-md-6 col-md-offset-3">
- 				<h1 class="text-center">Input Laporan Kerusakan</h1>
- 				<br><br>
- 				<form action="<?= base_url('C_PelaporBencana/tambahLaporan')?>" method="POST">
+ <body class="fix-header">
+	<?php $this->load->view('menu_nav') ?>
+	<div id="wrapper">
+		<div id="page-wrapper">
+			<div class="container-fluid">
+				<div class="row bg-title">
+				<div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                    <h4 class="page-title">Input Laporan Kerusakan</h4> 
+                 </div>
+			</div>
+			<div class="row">
+				<div class="white-box">
+					<form action="<?= base_url('C_PelaporBencana/tambahLaporan')?>" method="POST" class="form-horizontal form-material">
  					<input type="hidden" name="id_bencana" value="<?=$id_bencana?>">
  					<input type="hidden" name="id_wilayah" value="<?=$this->session->userdata('id_wilayah')?>">
  					<input type="hidden" name="id_pengguna" value="<?=$this->session->userdata('id_pengguna');?>">
  					<input type="hidden" name="tanggal_laporan" value="<?php echo date('Y-m-d H:i:s');?>">
  					<div class="row-input">
  						<p>Objek Kerusakan</p>
- 						<select name="objek" id="" class="form-control">
+ 						<select name="objek" id="" class="form-control form-control-line">
  							<option value="Rumah">Rumah</option>
  							<option value="Fasilitas Pendidikan">Fasilitas Pendidikan</option>
  							<option value="Fasilitas Kesehatan">Fasilitas Kesehatan</option>
@@ -32,24 +37,26 @@
  					</div>
  					<div class="row-input">
  						<p>Persentase Kerusakan Komponen Struktur Bangunan</p>
- 						<input type="number" class="form-control" name="komponenStruktur">
+ 						<input type="number" class="form-control form-control-line" name="komponenStruktur">
  					</div>
  					<div class="row-input">
  						<p>Persentase Kerusakan Komponen Penunjang Bangunan</p>
- 						<input type="number" class="form-control" name="komponenPenunjang">
+ 						<input type="number" class="form-control form-control-line" name="komponenPenunjang">
  					</div>
  					<div class="row-input">
  						<p>Lokasi</p>
- 						<textarea name="lokasi"  cols="30" rows="10" class="form-control"></textarea>
+ 						<textarea name="lokasi" class="form-control form-control-line"></textarea>
  					</div>
  					<div class="row-input">
  						<input type="submit" class="btn btn-primary">&nbsp
  						<a href="<?= base_url('C_PelaporBencana/viewBencanaPelapor?id_bencana='.$id_bencana)?>" class="btn btn-danger">Cancel</a>
  					</div>
  				</form>
- 				
-
- 			</div>
- 		</div>
- 	</div>
+				</div>
+			</div>
+			</div>
+			
+		</div>
+	</div>
+ 	
  </body>
